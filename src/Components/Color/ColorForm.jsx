e32c2d837a6c07cb295d7968296c9b27e8a67fed
Anna Schemmel <anna.schemmel@gmx.de>
2025-09-01 18:00:41 +0200

    Add: Form submit and color picker values in text field

--- src/Components/Color/ColorForm.jsx
+++ src/Components/Color/ColorForm.jsx
@@ -1,18 +1,36 @@
-export default function Colorform() {
-  return (
-    //  function handleSubmit(event) {
-    //  event.preventDefault()
-    //  }
+import { useState } from "react";
 
-    <form className="colorform">
+export default function Colorform() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("submitted form");
+  }
+  const [colorHex, setColorHex] = useState("#f43497");
+  //console.log(colorHex);
+  const [contrastText, setContrastText] = useState("#140f45");
+
+  return (
+    <form className="colorform" onSubmit={handleSubmit}>
       <label htmlFor="role">Role</label>
       <input type="text" name="role" />
       <label htmlFor="hex">Hex</label>
-      <input type="text" name="hex" />
-      <input type="color" name="hexColor" id="hexColor" />
+      <input type="text" name="hex" value={colorHex} />
+      <input
+        type="color"
+        name="hexColor"
+        id="hexColor"
+        value={colorHex}
+        onChange={(e) => setColorHex(e.target.value)}
+      />
       <label htmlFor="contrastText">Contrast Text</label>
-      <input type="text" name="contrastText" />
-      <input type="color" name="contrastTextColor" id="hex-color" />
+      <input type="text" name="contrastText" value={contrastText} />
+      <input
+        type="color"
+        name="contrastTextColor"
+        id="hex-color"
+        value={contrastText}
+        onChange={(e) => setContrastText(e.target.value)}
+      />
       <button type="submit">Add color</button>
     </form>
   );
