c6dcbd3a57b24270822607e5da1bd643bfab43c6
Anna Schemmel <anna.schemmel@gmx.de>
2025-09-02 16:41:41 +0200

    Add: Button group and hidden state to confirm delete

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,7 +1,10 @@
 import "./Color.css";
+import { useState } from "react";
 
 export default function Color({ color, onDeleteColor, id }) {
   //console.log("Find Issue 1");
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
     <div
       className="color-card"
@@ -13,9 +16,18 @@ export default function Color({ color, onDeleteColor, id }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
-      <button title="delete movie" onClick={() => onDeleteColor(id)}>
-        Delete
-      </button>
+      {isVisible && (
+        <button onClick={() => setIsVisible(!isVisible)}>Delete</button>
+      )}
+      {!isVisible && (
+        <div>
+          <p className="color-card-headline">Really delete?</p>
+          <button onClick={() => setIsVisible(!isVisible)}>Cancel</button>
+          <button title="delete movie" onClick={() => onDeleteColor(id)}>
+            Delete
+          </button>
+        </div>
+      )}
     </div>
   );
 }
